c5330d17f0ac60694e164ce5d294ad4722fa5516
Renan Oliveira <renan368@hotmail.com>
2022-08-26 21:35:11 -0300

    feat: Adicionado nova entidade medico e o controller para consumir

---
+++ BackEnd/ConsultMedica/src/main/java/unisales/br/ConsultMedica/model/MedicoRepository.java
@@ -0,0 +1,11 @@
+package unisales.br.ConsultMedica.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    
+    
+    
+}
